1e0b13c591f50390929f3bba565c8f21f0eda820
samdevbr <samuel.oliveira@getrak.com.br>
2019-02-09 02:12:40 -0200

    Creating service provider for the package and macros

---
+++ src/BigreportServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Samdevbr\Bigreport;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
+
+class BigreportServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes(
+                [
+                    __DIR__.'/../config/bigreport.php' => config_path('bigreport.php')
+                ],
+                'config'
+            );
+        }
+
+        $this->mergeConfigFrom(__DIR__.'/../config/bigreport.php', 'bigreport');
+
+        Builder::macro('export', function (string $filename = '', array $headings = [], int $chunkSize = 1000) {
+            $bigReport = new Bigreport($this, $filename, $headings, $chunkSize);
+
+            return $bigReport->export();
+        });
+    }
+}
